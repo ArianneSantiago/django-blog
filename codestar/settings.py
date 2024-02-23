800307a2179f06f9d2789cfd3a8c05c5603f3f99
Arianne Santiago <ariannesantiago05@gmail.com>
2024-02-23 13:52:13 +0000

    Add filter to Postlist

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-@l3ms76&b4bn$*jai5-zhzsmoog_s-8417*fyr+(d7c2@btp$w
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-ariannesanti-djangoblog-yzavahtp72k.ws-eu108.gitpod.io','.herokuapp.com']
 
